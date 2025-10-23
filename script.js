4d34f5814c3c6c76735499780922fe3fbf42bf2e
adhanani05 <adityadhanani05@gmail.com>
2025-10-23 18:27:50 -0400

    Week 3 pre-lesson

---
+++ script.js
@@ -0,0 +1,30 @@
+// ==============================
+// Live-coding Exercise 1: JavaScript Basics
+// ==============================
+// GOAL: Practice arrays and loops by logging multiple messages to the console.
+// Scenario: We have a list of positive coding reminders to print one by one.
+// Steps:
+// 1. Create an array of messages (strings).
+// 2. Use a for loop to log each message in the console.
+
+
+// ==============================
+// Live-coding Exercise 2: Interacting with the DOM
+// ==============================
+// GOAL: Add interactivity to the webpage by responding to a button click.
+// Scenario: When the user clicks the "Say Hello" button, display a message below it.
+// Steps:
+// 1. Set the button and paragraph elements using document.getElementById() into variables.
+// 2. Add an event listener for the "click" event.
+// 3. Inside the function, update the paragraph’s textContent.
+
+
+// ==============================
+// Live-coding Exercise 3: Async / Promises
+// ==============================
+// GOAL: Reveal a hidden message after a short delay using setTimeout().
+// Steps:
+// 1. Select the button and paragraph and set it to variables
+// 2. Add an event listener for "click"
+// 3. Show temporary text like “Thinking...”
+// 4. After 2 seconds, change it to the final message
